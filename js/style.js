18a661a69cc358d7d4259ea89361ceb800cd468f
Rodrigo Ezequiel Romano <rodrigoprograma22@gmail.com>
2024-10-22 19:26:37 -0300

    Se modifico el diseño, paleta de colores, fuentes, iconos.

--- js/style.js
+++ js/style.js
@@ -1,6 +1,15 @@
-window.addEventListener("scroll", function () {
-    var nav = document.querySelector("nav");
-    nav.classList.remove("bg-transparent");
-    nav.classList.toggle("fondo-dark", window.scrollY > 0);
+// window.addEventListener("scroll", function () {
+//     var nav = document.querySelector("nav");
+//     nav.classList.remove("bg-transparent");
+//     nav.classList.toggle("fondo-dark", window.scrollY > 0);
+//   });
+window.addEventListener("load", () => {
+    const progress = document.getElementById("progress");
+    requestAnimationFrame(update);
   });
-  
+  function update() {
+    progress.style.width = `${
+      (window.scrollY / (document.body.scrollHeight - window.innerHeight)) * 100
+    }%`;
+    requestAnimationFrame(update);
+  }
